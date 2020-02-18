ae7289b21020e4f440be57c735e29fb2a88b581a
SpiralP <spiral.palmer@gmail.com>
2020-02-18 00:54:19 -0800

    let's target browserlist's "default" and polyfill async with regenerator runtime

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 // gross fix for winston in browser
 // https://github.com/winstonjs/winston/issues/1354#issuecomment-426433071
 import "setimmediate";
 
-// import css files
-import "normalize.css/normalize.css";
-// <!-- blueprint-icons.css file must be included alongside blueprint.css! -->
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-import "@blueprintjs/core/lib/css/blueprint.css";
+// // import css files
+// import "normalize.css/normalize.css";
+// // <!-- blueprint-icons.css file must be included alongside blueprint.css! -->
+// import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+// import "@blueprintjs/core/lib/css/blueprint.css";
 
 ReactDOM.render(<App />, document.querySelector("#container"));
