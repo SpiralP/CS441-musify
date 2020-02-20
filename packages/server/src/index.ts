64a720f19362fe3d374767f69adb829a223ece76
SpiralP <spiral.palmer@gmail.com>
2020-02-20 10:54:26 -0800

    try using lerna to handle our multiple packages

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -38,7 +38,7 @@ async function start() {
     })
   );
 
-  app.use(express.static("../dist"));
+  app.use(express.static("../client/dist"));
 
   // pass access_token from authorization code handoff straight to client :JOY:
   app.get("/callback", function(req, res, next) {
