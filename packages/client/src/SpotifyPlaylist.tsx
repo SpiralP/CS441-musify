4761e122a2ba290a8eb197c4886bcb020b367fb0
SpiralP <spiral.palmer@gmail.com>
2020-02-24 03:21:05 -0800

    try using the weird loading update callback

--- packages/client/src/SpotifyPlaylist.tsx
+++ packages/client/src/SpotifyPlaylist.tsx
@@ -29,66 +29,81 @@ export default class SpotifyPlaylist extends React.PureComponent<
     return (
       <Loader
         promise={() =>
-          api
-            .getPlaylist(playlistId)
-            .then(({ body }) => {
-              const tracks = body.tracks.items;
-              const trackIds = tracks.map((track) => track.track.id);
-              return { body, trackIds };
-            })
-            .then(async ({ body, trackIds }) => {
-              const tracks = await Promise.all(
-                trackIds.map((id) => api.getTrack(id))
-              );
-
-              return { body, trackIds, tracks };
-            })
+          api.getPlaylist(playlistId).then(({ body }) => {
+            const tracks = body.tracks.items;
+            const trackIds = tracks.map((track) => track.track.id);
+            return { body, trackIds };
+          })
         }
         renderError={(error) => `SpotifyPlaylist error: ${error}`}
         renderLoading={() => "loading playlist"}
-        renderSuccess={({ body, tracks }) => {
-          const { name, external_urls } = body;
+        renderSuccess={({ body, trackIds }) => (
+          <Loader
+            promise={async (update) => {
+              const tracks: SpotifyApi.SingleTrackResponse[] = [];
+              for (let index = 0; index < trackIds.length; index++) {
+                const id = trackIds[index];
 
-          const urlToTrack: {
-            [url: string]: SpotifyApi.SingleTrackResponse;
-          } = {};
-          const trackUrls = tracks.map(({ body }) => {
-            const url = body.preview_url;
-            urlToTrack[url] = body;
-            return url;
-          });
+                update(index / trackIds.length);
+                const track = await api.getTrack(id);
+                tracks.push(track.body);
+              }
 
-          return (
-            <div>
-              <h3>
-                Playlist <a href={external_urls.spotify}> {name}</a>
-              </h3>
-              <AudioTransitioner
-                urls={trackUrls}
-                autoPlay={autoPlay}
-                render={(url) => {
-                  if (!url) {
-                    return null;
-                  }
+              return { body, tracks };
+            }}
+            renderError={(error) => `SpotifyPlaylist track error: ${error}`}
+            renderLoading={(updateValue: number | null) =>
+              `loading playlist ${
+                updateValue ? Math.floor(updateValue * 100) : 0
+              }%`
+            }
+            renderSuccess={({ body, tracks }) => {
+              const { name, external_urls } = body;
+              const urlToTrack: {
+                [url: string]: SpotifyApi.SingleTrackResponse;
+              } = {};
 
-                  const { external_urls, name, artists } = urlToTrack[url];
-                  return (
-                    <h3>
-                      <a href={external_urls.spotify}> {name}</a>
-                      {" by "}
-                      {artists.map(({ name, external_urls }, i) => [
-                        i > 0 && ", ",
-                        <a key={i} href={external_urls.spotify}>
-                          {name}
-                        </a>,
-                      ])}
-                    </h3>
-                  );
-                }}
-              />
-            </div>
-          );
-        }}
+              const trackUrls = tracks.map(
+                (track: SpotifyApi.SingleTrackResponse) => {
+                  const url = track.preview_url;
+                  urlToTrack[url] = track;
+                  return url;
+                }
+              );
+
+              return (
+                <div>
+                  <h3>
+                    Playlist <a href={external_urls.spotify}> {name}</a>
+                  </h3>
+                  <AudioTransitioner
+                    urls={trackUrls}
+                    autoPlay={autoPlay}
+                    render={(url) => {
+                      if (!url) {
+                        return null;
+                      }
+
+                      const { external_urls, name, artists } = urlToTrack[url];
+                      return (
+                        <h3>
+                          <a href={external_urls.spotify}> {name}</a>
+                          {" by "}
+                          {artists.map(({ name, external_urls }, i) => [
+                            i > 0 && ", ",
+                            <a key={i} href={external_urls.spotify}>
+                              {name}
+                            </a>,
+                          ])}
+                        </h3>
+                      );
+                    }}
+                  />
+                </div>
+              );
+            }}
+          />
+        )}
       />
     );
   }
