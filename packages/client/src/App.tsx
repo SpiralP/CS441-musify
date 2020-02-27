d93122dc23a3b38187b4c0805fe6e1f0ccabc8af
Amanda <chan041@cougars.csusm.edu>
2020-02-27 15:20:37 -0800

    hi spiral

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -35,42 +35,7 @@ export default class App extends React.PureComponent<{}, AppState> {
 
     return (
       <div>
-        <Loader
-          promise={() => Camera.setup()}
-          renderError={(error) => `Camera error: ${error}`}
-          renderLoading={() => "loading camera"}
-          renderSuccess={(camera) => (
-            <CameraSnapshotter
-              camera={camera}
-              interval={4000}
-              onCapture={(blob) => {
-                this.setState({
-                  currentState: { type: "askingServer" },
-                });
-
-                getFacesFromData(blob).then((data) => {
-                  console.log(data);
-
-                  this.setState({
-                    currentState: { type: "data", data },
-                  });
-                });
-              }}
-            />
-          )}
-        />
-
-        {currentState.type === "data" ? (
-          <Mooder data={currentState.data} />
-        ) : null}
-
-        <SpotifyApi
-          render={() => (
-            <Autoplay>
-              <SpotifyPlaylist autoPlay playlistId="0WfvdlPZunjRMlTWpZdK1t" />
-            </Autoplay>
-          )}
-        />
+        <h1 className="titlename">jflkajfaksljfl</h1>
       </div>
     );
   }
